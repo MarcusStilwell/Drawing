0c29792d8b42ce651512573d57e7e004540d86e8
MarcusStilwell <marcusstilwell@icloud.com>
2021-10-27 15:31:46 -0700

    Added image for border using ImagePaint

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -71,18 +71,9 @@ struct ContentView: View {
     @State private var petalWidth = 100.0
     
     var body: some View {
-        VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(Color.red, style: FillStyle(eoFill: true))
-            
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
-        }
+            Text("Hellow World")
+                .frame(width: 300, height: 300)
+                .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
     }
 }
 
